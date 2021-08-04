b1add2bc87d7e12eaf27bdea1392927ea0ee4e51
Jannes <hypercookie@gmx.de>
2021-08-04 17:05:56 +0200

    Moved Stuff and Changed Paths

--- src/main/java/Properties/CurrentSongProperty.java
+++ src/main/java/Properties/CurrentSongProperty.java
@@ -1,19 +1,26 @@
 package Properties;
 
 import javafx.beans.binding.ObjectBinding;
-import javafx.beans.property.SimpleObjectProperty;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class CurrentSongProperty extends ObjectBinding<Song> {
 
     @Override
     protected Song computeValue() {
         Runtime rt = Runtime.getRuntime();
-        String[] commands = {"/Users/jannes/IdeaProjects/SkipMe/src/main/java/CurrentPlaying"};
+        URL res = getClass().getClassLoader().getResource("CurrentPlaying");
+        try {
+            assert res != null;
+            File f = Paths.get(res.toURI()).toFile();
+        String[] commands = {f.getAbsolutePath()};
         try {
             Process proc = rt.exec(commands);
 
@@ -22,6 +29,9 @@ public class CurrentSongProperty extends ObjectBinding<Song> {
             JSONObject j = new JSONObject(s);
             return new Song(j.getString("Title"),j.getString("Album"), j.getString("Artist"));
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        } catch (URISyntaxException e) {
             e.printStackTrace();
         }
         return null;
